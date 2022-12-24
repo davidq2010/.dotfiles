9c8f633f53b2a822afe5e96a91c0e87572415676
davidq2010 <davidq2010@gmail.com>
2022-12-24 01:47:22 -0500

    New mappings and options based on primeagen's video

--- config/nvim/lua/config/mappings.lua
+++ config/nvim/lua/config/mappings.lua
@@ -6,3 +6,16 @@ u.imap("<C-U>", "<C-G>u<C-U>")
 u.imap("<C-W>", "<C-G>u<C-W>")
 -- https://stackoverflow.com/a/662914
 u.nmap("<CR>", "<Cmd>nohlsearch<CR><CR>")
+
+-- move visual lines around as a chunk
+u.vmap("J", ":m '>+1<CR>gv=gv")
+u.vmap("K", ":m '<-2<CR>gv=gv")
+
+-- yank into system clipboard
+u.nmap("<leader>y", '"+y')
+u.vmap("<leader>y", '"+y')
+u.nmap("<leader>Y", '"+Y')
+
+-- delete to void register
+u.nmap("<leader>d", '"_d')
+u.vmap("<leader>d", '"_d')
